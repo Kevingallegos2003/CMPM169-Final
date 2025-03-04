0935336b831f1b07e27cf400dda4a5b336a17ed1
AshleyJKnapp <ashjknapp@gmail.com>
2025-03-04 15:24:45 -0800

    New sand colors New functions started for directional sand

--- Scenes/Gallery.js
+++ Scenes/Gallery.js
@@ -9,6 +9,9 @@ class Gallery extends Phaser.Scene {
         this.load.json('hitbox', 'Scenes/hitbox.json');
         this.load.image("hourglass", 'Scenes/hourglass.png');
         this.load.image("sand", 'Scenes/sand.png');
+        this.load.image("redSand", 'Scenes/sandR.png');
+        this.load.image("yellowSand", 'Scenes/sandY.png');
+        this.load.image("blueSand", 'Scenes/sandB.png');
   
     }
 
@@ -26,16 +29,36 @@ class Gallery extends Phaser.Scene {
         glass.setBounce(0.7);
 
         for (let i = 0; i < 32; i++)
-            {
-    
-                my.g = this.matter.add.sprite(400, 0, 'sand').setAngle(20);
-                my.g.setCircle();
-                // my.g.setScale();
-
-            }
+        {
+                // for the prototype, lets consider just picing a random color/sprite from a list?
+                
+                // this.genDownwardsSand('sand');
+                this.genUpwardsSand('sand');
+        }
     }
 
     update(){
+
+    }
+
+    genDownwardsSand(spriteImg){
+        this.my.g = this.matter.add.sprite(400, 0, spriteImg).setAngle(20);
+        this.my.g.setCircle();
+        // this.my.g.setScale();
+    }
+
+    genUpwardsSand(spriteImg){
+        let g = this.matter.add.sprite(400, 600, spriteImg);
+        g.setCircle();
+        let p = this.matter.bodies.circle(400, 600, 5, {
+            render: {
+                sprite: g,
+            },
+            force: {x: 0, y: -500},
+        });
+        // console.log(typeof(p));
+        this.matter.body.setMass(p, -50); // idk why this and other functions dont appear to do anything????
+        // this.my.g.setScale();
     }
     
 }
