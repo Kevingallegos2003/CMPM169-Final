a3e9ae13f2beec17614ed6712c3b11fcad2d5e7b
Kevingallegos2003 <kevingallegos2003@gmail.com>
2025-03-17 20:04:51 -0700

    Keys disappeared

--- js/open-ai.js
+++ js/open-ai.js
@@ -8,7 +8,7 @@ export const getEmotion = async (message) => {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-            "Authorization": ''
+            "Authorization": 'Bearer'
         },
         body: JSON.stringify({
             model: "gpt-4o-mini",
